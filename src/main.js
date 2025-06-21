b8081f1749def3b832b605c401af264d3fed4329
xWickz <wickzcs@gmail.com>
2025-06-21 16:54:09 -0400

    Mejorar gestión de autenticación: se añade verificación de sesión en main.js y se actualizan mensajes de estado en Main.vue con enlaces de navegación.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css';
 import router from './router';
+import { supabase } from './lib/supabaseClient';
+import { store } from './modules/Auth/store/store';
 
 const app = createApp(App);
+
+supabase.auth.getSession().then(({ data: { session } }) => {
+    store.state.user = session?.user ?? null;
+});
+
+supabase.auth.onAuthStateChange((event, session) => {
+  if (event === "SIGNED_OUT") {
+    store.state.user = null;
+    router.push('/login');
+    console.info("La sesión fue destruida del store.");
+  } else if (event === "SIGNED_IN") {
+    store.state.user = session?.user ?? null;
+    router.push('/');
+    console.info("La sesión fue guardada en el store.");
+  }
+});
 
 app.use(router);
 app.mount('#app');
--- src/modules/Main/Main.vue
+++ src/modules/Main/Main.vue
@@ -3,13 +3,14 @@
   <section class="bg-gray-100 h-screen p-2">
 
     <!-- check if user is available in the store.js (show auth component if not) -->
-    <Auth v-if="!store.state.user"/>
     <div v-if="!store.state.user">
-      no has iniciado sesion jefe
+      no has iniciado sesion jefe, <router-link to="/login">inicia aqui</router-link>
     </div>
 
     <!-- if user is logged in, show any component or what else -->
     <div v-else class="p-5">
+      te acordas de no has iniciado sesion jefe? ya no se ve
+      <br/>
       Bienvenido <span class="font-bold">{{ store.state.user.email }}</span>!
       <br/>
     <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" @click="logout">
@@ -48,21 +49,7 @@ export default {
       } else {
         store.state.user = sessionData?.session?.user ?? null;
       }
-
-    supabase.auth.onAuthStateChange((event, session) => {
-        if (event === "SIGNED_OUT") {
-          store.state.user = null;
-          router.push('/login'); // send user to login if signed out
-          console.info("La sesion fue destruida del store.");
-        } else {
-          store.state.user = session?.user ?? null;
-          router.push('/'); // send user to main page if signed in
-          console.info("La sesion fue guardada en el store.");
-        }
-      });
     });
-
-
 
     const logout = async () => {
       await supabase.auth.signOut();
@@ -75,5 +62,4 @@ export default {
   }
 };
 
-console.log(store.state.user.id)
 </script>
