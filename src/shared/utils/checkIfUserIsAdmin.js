e3769a469e6c0336b74c6508ab94425f6414da2f
xWickz <wickzcs@gmail.com>
2025-06-28 11:18:02 -0400

    feat: mejorar la verificación de usuario administrador al agregar un retorno temprano

--- src/shared/utils/checkIfUserIsAdmin.js
+++ src/shared/utils/checkIfUserIsAdmin.js
@@ -3,7 +3,8 @@ import { supabase } from '../../lib/supabaseClient';
 
 export async function checkUserAdmin() {
   const { data: userData } = await supabase.auth.getUser();
-
+  if(!userData?.user) return false;
+  
   const { data, error } = await supabase
     .from("profiles")
     .select("role")
